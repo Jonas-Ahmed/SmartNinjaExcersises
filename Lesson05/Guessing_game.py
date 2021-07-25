2c8b34718bb90f2a1e78d4afa986c57d08551c1f
Jonas-Ahmed <87078211+Jonas-Ahmed@users.noreply.github.com>
2021-07-25 08:12:59 +0200

    Add files via upload

--- Lesson05/Guessing_game.py
+++ Lesson05/Guessing_game.py
@@ -7,24 +7,27 @@ secret_no = random.randint(1, 30)
 
 wrong_guess = []
 
-with open("score_list.json", "r") as score_file:
-    score_list = json.loads(score_file.read())
-
-
-def play_easy():
+def play_easy(name):
     while True:
-        name1 = name
         attempts = 0
+        score_list = scores()
         guess = int(input("Guess the secret number (between 1 and 30): "))
         attempts += 1
 
         if guess == secret_no:
-            score_list.append({"Attempts": attempts, "date": str(datetime.datetime.now()), "name": name1, "Secret_no.": secret_no,
+            score_list.append({"attempts": attempts, "date": str(datetime.datetime.now()), "name": name, "Secret_no.": secret_no,
             "wrong_guess": wrong_guess})
 
             print("You've guessed it - congratulations! It's number " + str(secret_no))
             print("Attempts needed: " + str(attempts))
-            break
+
+            with open("score_list.json", "w") as score_file:
+                score_file.write(json.dumps(score_list))
+
+            again = input("Would you like to try again y/n ").lower()
+            if again == "n":
+                break
+
         elif guess > secret_no:
             print("Your guess is not correct... try something smaller")
         elif guess < secret_no:
@@ -33,18 +36,18 @@ def play_easy():
         wrong_guess.append(guess)
 
 
-def play_hard():
+def play_hard(name):
     while True:
-        name1 = name
         attempts = 0
+        score_list = scores()
         guess = int(input("Guess the secret number (between 1 and 30): "))
         attempts += 1
 
         if guess == secret_no:
-            score_list.append({"Attempts": attempts, "date": str(datetime.datetime.now()), "name": name1, "Secret_no.": secret_no,"wrong_guess": wrong_guess})
+            score_list.append({"attempts": attempts, "date": str(datetime.datetime.now()), "name": name, "Secret_no.": secret_no,"wrong_guess": wrong_guess})
 
-            #with open("score_list.json", "w") as score_file:
-            #    score_file.write(json.dumps(score_list))
+            with open("score_list.json", "w") as score_file:
+                score_file.write(json.dumps(score_list))
 
             print(f"you guessed correct, the no. is {secret_no}")
             print(f"You guessed {attempts} times")
@@ -60,21 +63,30 @@ def play_hard():
 
 
 def scores():
-    print(f"Top point: {score_list}")
+    with open("score_list.json", "r") as score_file:
+        score_list = json.loads(score_file.read())
+        return score_list
 
 
-def main():
-    name = input("What is your name ? ")
-    print(f"Hej {name}")
-    selection = input("Would you like to A) play the game easy mode, B) play the game hard mode, C) see the scores or D) quit?")
+def top_score():
+    score_list = scores()
+    new_score_list = sorted(score_list, key=lambda k: k["attempts"])[:3]
+    return new_score_list
 
-    if selection == "A":
-        play_easy()
-    elif selection == "B":
-        play_hard()
-#    elif selection == "C":
-        # scores()
+
+
+name = input("What is your name ? ")
+print(f"Hej {name}")
+selection = input("Would you like to A) play the game easy mode, B) play the game hard mode, C) see the scores or type anything else to quit?")
+
+while True:
+    if selection.upper() == "A":
+        play_easy(name)
+    elif selection.upper() == "B":
+        play_hard(name)
+    elif selection.upper() == "C":
+        for x in top_score():
+            print(str(x["attempts"]) + " attempts and date " + str(x["date"]))
     else:
-        "Break"
+        break
 
-main()
